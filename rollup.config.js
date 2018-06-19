eff0d3e1bad0220ad2800ca74f00f109c0e7b55e
ProtoD <prototype.digital@outlook.com>
2018-06-19 05:24:54 +0300

    Fixed package.json

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import pkg from './package.json';
 export default {
     input: 'lib/index.js',
     output: [
-        { file: pkg.module, format: 'es' },
+        { file: pkg.main, format: 'es' },
     ],
     plugins: [
     ]
